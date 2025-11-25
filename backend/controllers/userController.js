00b0b1bf0eeffeb679797d4bfb1267539a0d9ccc
Orchard <rjbatson1@gmail.com>
2025-11-26 08:36:29 +1100

    userController CRUD feature added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,51 @@
+const Models = require("../models");
+
 const getAllUsers = (req, res) => {
-    res.json({message: "get all users"})
-}
+  Models.User.find({})
+    .then((data) => res.send({ result: 200, data: data }))
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
 
 const createUser = (req, res) => {
-    res.json({message: "create a user"})
-}
+  const data = req.body;
+  console.log(data);
+  new Models.User(data)
+    .save()
+    .then((data) => res.send({ result: 200, data: data }))
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
 
 const updateUser = (req, res) => {
-    const id = req.params.id
-    res.json({message: `update user id: ${id}`})
-}
+  const data = req.body;
+  console.log(data);
+  const id = req.params.id;
+  Models.User.findByIdAndUpdate(id, data, {new: true})
+    .then(data => res.send({result: 200, data: data}))
+    .catch(err => {
+        console.log(err);
+        res.send({result: 500, error: err.message})
+    })
+};
 
 const deleteUser = (req, res) => {
-    const id = req.params.id
-    res.json({message: `delete user id: ${id}`})
-}
+  const id = req.params.id;
+  Models.User.findByIdAndDelete(id)
+    .then(data => res.send({result: 200, data: data}))
+    .catch(err => {
+        console.log(err);
+        res.send({result: 500, error: err.message})
+    })
+};
 
 module.exports = {
-    getAllUsers,
-    createUser,
-    updateUser,
-    deleteUser,
-}
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+};
