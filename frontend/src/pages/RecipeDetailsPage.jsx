5ad6395136b23f00b86b967c1883399c4acf7dd6
Orchard <rjbatson1@gmail.com>
2025-11-26 18:59:31 +1100

    update to recipeDetailsPage.jsx

--- frontend/src/pages/RecipeDetailsPage.jsx
+++ frontend/src/pages/RecipeDetailsPage.jsx
@@ -3,7 +3,6 @@ import RecipeDetails from "../components/RecipeDetails"
 export function RecipeDetailsPage() {
     return (
         <>
-            <h1>Recipe form</h1>
             <RecipeDetails />
         </>
     )
