fe01e62437843222a9dc704e9d52911b7ac02acd
Orchard <rjbatson1@gmail.com>
2025-11-29 15:42:32 +1100

    b-end register and login added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,33 +24,60 @@ const createUser = (req, res) => {
 const getUser = (req, res) => {
   const id = req.params.id;
   Models.User.findById(id)
-    .then((data) => res.send({result: 200, data: data}))
+    .then((data) => res.send({ result: 200, data: data }))
     .catch((err) => {
       console.log(err);
-      res.send({result: 500, error: err.message })
+      res.send({ result: 500, error: err.message });
     });
-}
+};
 
 const updateUser = (req, res) => {
   const data = req.body;
   console.log(data);
   const id = req.params.id;
-  Models.User.findByIdAndUpdate(id, data, {new: true})
-    .then(data => res.send({result: 200, data: data}))
-    .catch(err => {
-        console.log(err);
-        res.send({result: 500, error: err.message})
-    })
+  Models.User.findByIdAndUpdate(id, data, { new: true })
+    .then((data) => res.send({ result: 200, data: data }))
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
 };
 
 const deleteUser = (req, res) => {
   const id = req.params.id;
   Models.User.findByIdAndDelete(id)
-    .then(data => res.send({result: 200, data: data}))
-    .catch(err => {
-        console.log(err);
-        res.send({result: 500, error: err.message})
-    })
+    .then((data) => res.send({ result: 200, data: data }))
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const user = await Models.User.create(req.body);  // shotcut for save()
+    res.status(201).json({ message: "User registered!", user });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const user = await Models.User.findOne({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+    res.json({
+      message: "Login successful",
+      user: { _id: user._id, username: user.username },
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 module.exports = {
@@ -59,4 +86,6 @@ module.exports = {
   getUser,
   updateUser,
   deleteUser,
+  registerUser,
+  loginUser,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const Controllers = require("../controllers")
+const Controllers = require("../controllers");
 
 // RESTful User CRUD routes
 router
-    .route("/")
-    .get(Controllers.userController.getAllUsers)
-    .post(Controllers.userController.createUser);
+  .route("/")
+  .get(Controllers.userController.getAllUsers)
+  .post(Controllers.userController.createUser);
 
-router  .route("/:id")
-    .get(Controllers.userController.getUser)
-    .put(Controllers.userController.updateUser)
-    .delete(Controllers.userController.deleteUser);
+router
+  .route("/:id")
+  .get(Controllers.userController.getUser)
+  .put(Controllers.userController.updateUser)
+  .delete(Controllers.userController.deleteUser);
+
+router.post("/register", Controllers.userController.registerUser);
+router.post("/login", Controllers.userController.loginUser);
 
 module.exports = router;
